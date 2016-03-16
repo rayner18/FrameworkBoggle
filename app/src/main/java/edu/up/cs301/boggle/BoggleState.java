349687e1ded4b7241952cc8ad03f0c89df7f9628
Michael <waitt18@up.edu>
2016-03-15 22:02:54 -0700

    Michael's Additions

--- app/src/main/java/edu/up/cs301/boggle/BoggleState.java
+++ app/src/main/java/edu/up/cs301/boggle/BoggleState.java
@@ -1,20 +1,12 @@
 package edu.up.cs301.boggle;
 
 
-import android.content.res.Resources;
-
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 import edu.up.cs301.game.infoMsg.GameState;
 
@@ -30,18 +22,20 @@ public class BoggleState extends GameState {
     private int playerTurn; //tells which players turn it is
     private int player1Score;
     private int player2Score;
-    private ArrayList<String> currentWord; //the current word the player is making
+    private String currentWord; //the current word the player is making
     private ArrayList<String> wordBank; //the current words in the word bank
     private boolean timer; //true if the timer is running, false if timer has stopped
+    private String[][] gameBoard = new String[4][4];
+
+
 
     public BoggleState() {
         playerTurn = 0;
         player1Score = 0;
         player2Score = 0;
         currentWord = null;
-        wordBank = null;
         timer = true;
-
+        wordBank = new ArrayList<String>();
     }
 
     public BoggleState(BoggleState state) {
@@ -51,7 +45,6 @@ public class BoggleState extends GameState {
         currentWord = state.currentWord;
         wordBank = state.wordBank;
         timer = state.timer;
-
     }
 
     public boolean isTimer() {
@@ -61,6 +54,10 @@ public class BoggleState extends GameState {
     public void setTimer(boolean timer) {
         this.timer = timer;
     }
+
+    public String[][] getGameBoard() {return gameBoard;}
+
+    public void setGameBoard(String[][] gameBoard) {this.gameBoard = gameBoard;}
 
     public int getPlayerTurn() {
         return playerTurn;
@@ -86,11 +83,11 @@ public class BoggleState extends GameState {
         this.player2Score = player2Score;
     }
 
-    public ArrayList<String> getCurrentWord() {
+    public String getCurrentWord() {
         return currentWord;
     }
 
-    public void setCurrentWord(ArrayList<String> currentWord) {
+    public void setCurrentWord(String currentWord) {
         this.currentWord = currentWord;
     }
 
@@ -103,8 +100,8 @@ public class BoggleState extends GameState {
     }
 
 
-    public boolean wordLength(ArrayList<String> word) {
-        return word.size() >= 3;
+    public boolean wordLength(String word) {
+        return word.length() >= 3;
     }
 
     public void removeLetter(ArrayList<String> word) {
@@ -133,31 +130,95 @@ public class BoggleState extends GameState {
     }
 
     //public void inDictionary(File file, Resources resources){
-        public HashMap inDictionary(File file) throws FileNotFoundException {
+    public HashMap inDictionary(File file) throws FileNotFoundException {
 
-            Scanner scanner = new Scanner(new FileReader(file.getName()));
+        Scanner scanner = new Scanner(new FileReader(file.getName()));
 
-            HashMap<String, String> map = new HashMap<String, String>();
+        HashMap<String, String> map = new HashMap<String, String>();
 
-            while (scanner.hasNextLine()) {
-                String[] columns = scanner.nextLine().split(" ");
-                map.put(columns[0], columns[1]);
-            }
+        while (scanner.hasNextLine()) {
+            String[] columns = scanner.nextLine().split(" ");
+            map.put(columns[0], columns[1]);
+        }
 
-           // System.out.println(map);
-            return map;
+        // System.out.println(map);
+        return map;
     }
 
     public boolean wordAvailable() throws FileNotFoundException {
         File file1 = new File(".");
-        boolean file = findFile(file1,"words.txt");
+        boolean file = findFile(file1, "words.txt");
         return file;
         //HashMap map = inDictionary(file);
 
 
-
     }
 
+    /**
+     * Updates the score based on the length of the word entered
+     *
+     * @param word the word the user has submitted
+     */
+    public void updateScore(String word) {
+        int score = 0;
+
+        //Check to see how long the word is
+        if (word.length() <= 4 && word.length() >= 3) {
+            score = 1;
+        }
+
+        else if (word.length() == 5) {
+            score = 2;
+        }
+
+        else if (word.length() == 6) {
+            score = 3;
+        }
+
+        else if (word.length() == 7) {
+            score = 5;
+        }
+
+        else if (word.length() >= 8) {
+            score = 11;
+        }
+
+        //Update the player's score
+        player1Score = score;
+    }
+
+    /**
+     * Rotates the board
+     *
+     * @param board two dimensional array representing the board
+     */
+    public void rotateBoard(String[][] board) {
+        String[][] tmp = new String[4][4];
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 4; ++j) {
+                tmp[i][j] = board[4 - j - 1][i]; //Rotates the board
+            }
+        }
+
+        /**
+         * External Citation
+         *
+         * Date: 15 March 2016
+         * Problem: Didn't know how to rotate a two dimensional array.
+         * Resource: http://stackoverflow.com/questions/42519/how-do-you-rotate-
+         * a-two-dimensional-array
+         * Solution: Used the sample code from this post.
+         */
+
+        //Copies the rotated board to the existing board
+        gameBoard = tmp;
+    }
+
+    public void addToWordBank(String word) {
+        //Adds it to the arrayList
+         wordBank.add(word);
+    }
 
 
 }
--- app/src/test/java/edu/up/cs301/boggle/BoggleStateTest.java
+++ app/src/test/java/edu/up/cs301/boggle/BoggleStateTest.java
@@ -1,16 +1,19 @@
 package edu.up.cs301.boggle;
 
-import java.util.regex.Pattern;
-import junit.framework.Assert;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
 
 import org.junit.Test;
 
 /**
- * Created by Jacob on 3/15/2016.
+ * JUnit Test for testing the functionality of the BoggleState class
+ *
+ *
+ * @version 15 March 2016
+ *
+ * @author Michael Waitt
+ * @author Jacob Kirby
+ * @author Charles Rayner
  */
 public class BoggleStateTest {
 
@@ -39,5 +42,83 @@ public class BoggleStateTest {
     public void wordAvailable() throws Exception {
         BoggleState state = new BoggleState();
         boolean file2 = state.wordAvailable();
+    }
+
+    @Test
+    public void testUpdateScore() throws Exception {
+        BoggleState state = new BoggleState();
+        String longWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
+        state.updateScore(longWord);
+        assertEquals(state.getPlayer1Score(), 11);
+
+        String littleWord = "hue";
+        state.updateScore(littleWord);
+        assertEquals(state.getPlayer1Score(), 1);
+
+        String fourLetter = "goku";
+        state.updateScore(fourLetter);
+        assertEquals(state.getPlayer1Score(), 1);
+
+        String fiveLetter = "hello";
+        state.updateScore(fiveLetter);
+        assertEquals(state.getPlayer1Score(), 2);
+
+        String sixLetter = "pizzaz";
+        state.updateScore(sixLetter);
+        assertEquals(state.getPlayer1Score(), 3);
+
+        String sevenLetter = "jacuzzi";
+        state.updateScore(sevenLetter);
+        assertEquals(state.getPlayer1Score(), 5);
+    }
+
+    @Test
+    public void testRotateBoard() throws Exception {
+        BoggleState state = new BoggleState();
+        String[][] gameBoard = state.getGameBoard();
+        state.rotateBoard(gameBoard);
+        gameBoard[0][0] = "a";
+        gameBoard[0][1] = "a";
+        gameBoard[0][2] = "a";
+        gameBoard[0][3] = "a";
+        gameBoard[1][0] = "b";
+        gameBoard[1][1] = "b";
+        gameBoard[1][2] = "b";
+        gameBoard[1][3] = "b";
+        gameBoard[2][0] = "c";
+        gameBoard[2][1] = "c";
+        gameBoard[2][2] = "c";
+        gameBoard[2][3] = "c";
+        gameBoard[3][0] = "d";
+        gameBoard[3][1] = "d";
+        gameBoard[3][2] = "d";
+        gameBoard[3][3] = "d";
+
+        assertEquals(gameBoard[3][3], "d");
+        assertEquals(gameBoard[2][2], "c");
+        assertEquals(gameBoard[1][1], "b");
+        assertEquals(gameBoard[0][0], "a");
+    }
+
+    @Test
+    public void testAddToWordBank() throws Exception {
+        BoggleState state = new BoggleState();
+        String longWord = "pneumonoultramicroscopicsilicovolcanoconiosis";
+        state.addToWordBank(longWord);
+        assertEquals(state.getWordBank().get(0), longWord);
+
+        String testString = "test";
+        state.addToWordBank(testString);
+        assertEquals(state.getWordBank().get(1), testString);
+
+        String testString1 = "test";
+        state.addToWordBank(testString1);
+        assertEquals(state.getWordBank().get(2), testString1);
+
+        String testString2 = "test";
+        state.addToWordBank(testString2);
+        assertEquals(state.getWordBank().get(3), testString2);
+
+        assertEquals(state.getWordBank().size(), 4);
     }
 }
